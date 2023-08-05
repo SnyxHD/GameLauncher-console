6b3593534e754304df6bbe0e0f384116fe26098e
TF_Respect <kd8ogdtfrespect@gmail.com>
2023-08-05 00:43:11 -0400

    general rework
    using namespace std for readability
    general function cleanup
    variable name consistency

--- Launcher/src/headers/GameManager.cpp
+++ Launcher/src/headers/GameManager.cpp
@@ -1,33 +1,35 @@
 #include "GameManager.h"
 
+using namespace std;
+
 // Constructor, we call checkForDir() so it checks if the given directory exist everytime the object is created
 GameManager::GameManager() { checkForDir(); };
 
 // Deconstructor for class, called when the class goes out of scope
-GameManager::~GameManager() { std::cout << "Destroyed Object" << std::endl; };
+GameManager::~GameManager() { cout << "Destroyed Object" << endl; };
 
-std::string config_path = "config/Game_Paths.json";
+string config_path = "config/Game_Paths.json";
 
 // Add a new game path to the JSON file
-void GameManager::addGame(const std::string& path, const std::string& name, const std::string& args)
+void GameManager::addGame(const string& path, const string& name, const string& args)
 {
     json jsonData = json::object();
-    std::fstream jsonFile(config_path); // Use the more generalized fstream so the variable can be reused for reading and writing
+    fstream jsonFile(config_path); // Use the more generalized fstream so the variable can be reused for reading and writing
 
     // Check if the file is open and valid
     if (jsonFile.is_open()) {
         // Read the content of the file into a string
-        std::string jsonString((std::istreambuf_iterator<char>(jsonFile)), std::istreambuf_iterator<char>());
+        string jsonString((istreambuf_iterator<char>(jsonFile)), istreambuf_iterator<char>());
 
         // Check if the file is empty
         if (!jsonString.empty()) {
             // Attempt to parse the content as JSON
             try {
                 jsonData = json::parse(jsonString);
-                std::cout << "Successfully loaded config file." << std::endl;
+                cout << "Successfully loaded config file." << endl;
             }
             catch (const json::parse_error& e) {
-                std::cerr << "Error parsing JSON: " << e.what() << std::endl;
+                cerr << "Error parsing JSON: " << e.what() << endl;
                 return;
             }
         }
@@ -36,7 +38,7 @@ void GameManager::addGame(const std::string& path, const std::string& name, cons
     }
 
     // Open config file as write
-    jsonFile.open(config_path, std::ios::out | std::ios::trunc);
+    jsonFile.open(config_path, ios::out | ios::trunc);
 
     if (jsonFile.is_open()) {
         // Add new game to jsonData
@@ -54,62 +56,61 @@ void GameManager::addGame(const std::string& path, const std::string& name, cons
         return;
     }
 
-    std::cout << "Could not open config file." << std::endl;
+    cout << "Could not open config file." << endl;
     return;
 }
 
 // Stores all the Data From the JSON file into a vector array of type JSON
 void GameManager::loadFromFile()
 {
-    // TODO: Load All The Games from Json file into a std::vector<>
-
-    _games.clear(); // Clear the existing games vector before loading from file.
+    _games.clear();
 
     try
     {
         json jsonData;
-        std::ifstream file("config/Game_Paths.json"); // Input file stream to read JSON data.
+        ifstream jsonFile("config/Game_Paths.json");
 
-        // Check if the file is empty or not readable
-        if (file.peek() == std::ifstream::traits_type::eof())
-        {
-            std::cout << "File Empty Or Not Readable!" << std::endl;
-            return; // Early return as there's nothing to read from the file.
-        }
+        // Check if the file is open and valid
+        if (jsonFile.is_open()) {
+            // Read the content of the file into a string
+            string jsonString((istreambuf_iterator<char>(jsonFile)), istreambuf_iterator<char>());
 
-        // Parse the JSON data from the file
-        file >> jsonData;
-        file.close();
-
-        // Check if the JSON data is an array
-        if (jsonData.is_object())
-        {
-            // Iterate through the array and populate the _games vector
-            for (auto it = jsonData.begin(); it != jsonData.end(); ++it)
-            {
-                const std::string& gameName = it.key();
-                const json& gameData = it.value();
-
-                json gameEntry;
-                gameEntry["game_name"] = gameName;
-                gameEntry["game_path"] = gameData.value("game_path", "");
-                gameEntry["command_args"] = gameData.value("command_args", "");
-                _games.emplace_back(gameEntry);
+            // Check if the file is empty
+            if (!jsonString.empty()) {
+                // Attempt to parse the content as JSON
+                try {
+                    jsonData = json::parse(jsonString);
+                    cout << "Successfully loaded config file." << endl;
+                }
+                catch (const json::parse_error& e) {
+                    cerr << "Error parsing JSON: " << e.what() << endl;
+                    return;
+                }
             }
+
+            jsonFile.close();
         }
-        else
+
+        // Iterate through the array and populate the _games vector
+        for (auto entry = jsonData.begin(); entry != jsonData.end(); ++entry)
         {
-            std::cout << "JSON data is not an object." << std::endl;
-            return; // Early return as the data format is incorrect.
+            const string& gameName = entry.key();
+            const json& gameData = entry.value();
+
+            json gameEntry;
+            gameEntry["game_name"] = gameName;
+            gameEntry["game_path"] = gameData.value("game_path", "");
+            gameEntry["command_args"] = gameData.value("command_args", "");
+            _games.emplace_back(gameEntry);
         }
     }
     catch (const json::parse_error& e)
     {
-        std::cout << "JSON Parse Error: " << e.what() << std::endl;
+        cout << "JSON Parse Error: " << e.what() << endl;
     }
-    catch (const std::exception& e)
+    catch (const exception& e)
     {
-        std::cout << "Exception: " << e.what() << std::endl;
+        cout << "Exception: " << e.what() << endl;
     }
 }
 
@@ -117,12 +118,12 @@ void GameManager::loadFromFile()
 void GameManager::printGames()
 {
     json jsonData;
-    std::fstream jsonFile(config_path); // Use the more generalized fstream so the variable can be reused for reading and writing
+    fstream jsonFile(config_path); // Use the more generalized fstream so the variable can be reused for reading and writing
 
     // Check if the file is open and valid
     if (jsonFile.is_open()) {
         // Read the content of the file into a string
-        std::string jsonString((std::istreambuf_iterator<char>(jsonFile)), std::istreambuf_iterator<char>());
+        string jsonString((istreambuf_iterator<char>(jsonFile)), istreambuf_iterator<char>());
 
         // Check if the file is empty
         if (!jsonString.empty()) {
@@ -131,16 +132,16 @@ void GameManager::printGames()
                 jsonData = json::parse(jsonString);
             }
             catch (const json::parse_error& e) {
-                std::cerr << "Error parsing JSON: " << e.what() << std::endl;
+                cerr << "Error parsing JSON: " << e.what() << endl;
                 return;
             }
         }
 
-        for (auto i = jsonData.begin(); i != jsonData.end(); ++i)
+        for (auto entry = jsonData.begin(); entry != jsonData.end(); ++entry)
         {
-            std::cout << i.key() << std::endl;
+            cout << entry.key() << endl;
         }
-        std::cout << "--------------------------------------" << std::endl;
+        cout << "--------------------------------------" << endl;
 
         jsonFile.close();
     }
@@ -150,51 +151,68 @@ void GameManager::loopThroughSaveArray()
 {
     for (int i = 0; i < _games.size(); i++)
     {
-        std::cout << _games[i] << "\n" << std::endl;
+        cout << _games[i] << endl;
     }
 }
 
 // Opens Path From JSON file
 // TODO: Handle empty paths, and throw and exception if the file is not an .exe
-bool GameManager::openGame(const std::string& gamename)
+bool GameManager::openGame(const string& gamename)
 {
     try
     {
         // TODO: Open File containing all saved games and search for game name and open it
-        json fileData;
-        std::ifstream file("config/Game_Paths.json");
+        json jsonData;
+        ifstream jsonFile("config/Game_Paths.json");
 
-        file >> fileData;
-        if (fileData.find(gamename) == fileData.end())
+        // Check if the file is open and valid
+        if (jsonFile.is_open()) {
+            // Read the content of the file into a string
+            string jsonString((istreambuf_iterator<char>(jsonFile)), istreambuf_iterator<char>());
+
+            // Check if the file is empty
+            if (!jsonString.empty()) {
+                // Attempt to parse the content as JSON
+                try {
+                    jsonData = json::parse(jsonString);
+                    cout << "Successfully loaded config file." << endl;
+                }
+                catch (const json::parse_error& e) {
+                    cerr << "Error parsing JSON: " << e.what() << endl;
+                    return;
+                }
+            }
+
+            jsonFile.close();
+        }
+
+        if (!jsonData.contains(gamename))
         {
-            std::cout << "Game Not Found!" << std::endl;
+            cout << "Game Not Found!" << endl;
             return false;
         }
 
-        const std::string tempArgs = fileData[gamename].value("command_args", "");
-        const std::string tempPath = fileData[gamename].value("game_path", "");
+        const string tempArgs = jsonData[gamename].value("command_args", "");
+        const string tempPath = jsonData[gamename].value("game_path", "");
 
         if (tempPath.empty())
         {
-            std::cout << "Game Path not specified for '" << gamename << "'" << std::endl;
+            cout << "Game Path not specified for '" << gamename << "'" << endl;
             return false;
         }
 
-        const wchar_t* convertedArgs = convertStrToLPCWSTR(tempArgs); // Converts std::string to type const wchar_t* | LPCWSTR
-        const wchar_t* convertedPath = convertStrToLPCWSTR(tempPath); // Converts std::string to type const wchar_t* | LPCWSTR
-
-
-        //ShellExecute(NULL, L"open", convertedPath, NULL, NULL, SW_SHOWNORMAL);
+        const wchar_t* convertedArgs = convertStrToLPCWSTR(tempArgs);
+        const wchar_t* convertedPath = convertStrToLPCWSTR(tempPath);
 
         ShellExecute(NULL, L"open", convertedPath, convertedArgs, NULL, SW_SHOWNORMAL);
 
         if (!tempArgs.empty())
         {
-            std::wcout << "Opening " << convertedPath << " " << convertedArgs << ".... " << std::endl;
+            wcout << "Opening '" << convertedPath << "' with args: " << convertedArgs << " ... " << endl;
         }
         else
         {
-            std::wcout << "Opening " << convertedPath << ".... " << std::endl;
+            wcout << "Opening '" << convertedPath << "' ... " << endl;
         }
 
         delete[] convertedPath;
@@ -203,13 +221,13 @@ bool GameManager::openGame(const std::string& gamename)
     }
     catch (const json::parse_error& e)
     {
-        std::cout << "JSON Parse Error: " << e.what() << std::endl;
+        cout << "JSON Parse Error: " << e.what() << endl;
         return false;
     }
     catch (const json::type_error& e)
     {
-        std::cout << "JSON Type Error: " << e.what() << std::endl;
-        std::cout << "Game '" << gamename << "' Probably wasnt added correctly" << std::endl;
+        cout << "JSON Type Error: " << e.what() << endl;
+        cout << "Game '" << gamename << "' Probably wasnt added correctly" << endl;
         return false;
     }
 }
@@ -217,10 +235,10 @@ bool GameManager::openGame(const std::string& gamename)
 // Opens the JSON file and clears all the data in it
 void GameManager::deleteGames()
 {
-    std::ofstream file("config/Game_Paths.json", std::ios::trunc);
+    ofstream file("config/Game_Paths.json", ios::trunc);
     if (!file.is_open())
     {
-        std::cout << "ERROR OPENING FILE" << std::endl;
+        cout << "Could not clear games file." << endl;
         return;
     }
     file.close();
@@ -230,32 +248,32 @@ void GameManager::deleteGames()
 void GameManager::checkForDir()
 {
 
-    const std::wstring dirName = L"config";
+    const wstring dirName = L"config";
 
     try
     {
-        if (!std::filesystem::exists(dirName))
+        if (!filesystem::exists(dirName))
         {
-            if (std::filesystem::create_directory(dirName))
+            if (filesystem::create_directory(dirName))
             {
-                std::cout << "Directory Created" << std::endl;
+                cout << "Directory Created" << endl;
             }
             else
             {
-                std::cerr << "Failed to Create Directory!" << std::endl;
+                cerr << "Failed to Create Directory!" << endl;
             }
         }
     }
-    catch (const std::filesystem::filesystem_error& ex)
+    catch (const filesystem::filesystem_error& ex)
     {
-        std::cerr << "Error: " << ex.what() << std::endl;
+        cerr << "Error: " << ex.what() << endl;
     }
 }
 
 // Converts strings to wchar_t, Make sure to call delete[] on the converted string when done with it to avoid memory leaks
-const wchar_t* GameManager::convertStrToLPCWSTR(const std::string& tempStr)
+const wchar_t* GameManager::convertStrToLPCWSTR(const string& tempStr)
 {
-    std::wstring temp = std::wstring(tempStr.begin(), tempStr.end());
+    wstring temp = wstring(tempStr.begin(), tempStr.end());
     wchar_t* convertedStr = new wchar_t[temp.size() + 1];
     wcscpy_s(convertedStr, temp.size() + 1, temp.c_str());
 
